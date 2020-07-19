b278d382b99c9c9f72761c628aadfcbfa6fb7220
Jan <jan.urankar@gmail.com>
2020-07-19 13:42:18 +0200

    Basic structure added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
+import { AddCourseComponent } from './components/add-course/add-course.component';
+import { GetCoursesComponent } from './components/get-courses/get-courses.component';
+import { ShowCoursesComponent } from './components/show-courses/show-courses.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    AddCourseComponent,
+    GetCoursesComponent,
+    ShowCoursesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
